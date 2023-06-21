bc3d1a3fca78559e5a774f9200149bd2d72b9c09
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2023-06-21 18:31:40 +0300

    routs

--- screens/Home.js
+++ screens/Home.js
@@ -1,24 +1,105 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import 'react-native-gesture-handler';
 import React from 'react';
+import { MaterialCommunityIcons, AntDesign, Feather } from '@expo/vector-icons';
+import { HeaderBackButton } from '@react-navigation/elements';
+import PostScreen from '../screens/mainScreen/PostsScreen';
+import CreatePostScreen from '../screens/mainScreen/CreatePostsScreen';
+import Profile from '../screens/mainScreen/ProfileScreen';
 
-// import PostScreen from '../screens/mainScreen/PostsScreen';
-// import CreatePostScreen from '../screens/mainScreen/CreatePostsScreen';
-// import Profile from '../screens/mainScreen/ProfileScreen';
+const MainTab = createBottomTabNavigator();
 
-// const MainTab = createBottomTabNavigator();
-// createStackNavigator();
+export default function HomePage({ navigation }) {
+  return (
+    <MainTab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          bottom: 0,
+          minWidth: 375,
+          minHeight: 83,
+          paddingTop: 9,
+          paddingBottom: 34,
+          borderWidth: 1,
+          borderTop: 1,
+          heigth: 88,
+          borderTopWidth: 2,
 
-// export default function HomePage({ navigation }) {
-//   return (
-//     <NavigationContainer>
-//       <MainTab.Navigator>
-//         <MainTab.Screen name="Posts" component={PostScreen} />
-//         <MainTab.Screen name="CretePost" component={CreatePostScreen} />
-//         <MainTab.Screen name="UserProfile" component={Profile} />
-//       </MainTab.Navigator>
-//     </NavigationContainer>
-//   );
-// }
+          // border: 1,
+          // borderTopColor: '#BDBDBD',
+        },
+      }}
+      initialRouteName="Posts"
+    >
+      <MainTab.Screen
+        name="Posts"
+        component={PostScreen}
+        options={{
+          title: 'Публікації',
+          headerShown: true,
+          headerStyle: {},
+
+          tabBarShowLabel: false,
+          tabBarIcon: ({}) => {
+            return (
+              <MaterialCommunityIcons
+                name="view-grid-outline"
+                size={24}
+                color="rgba(33, 33, 33, 0.8)"
+              />
+            );
+          },
+        }}
+      />
+      <MainTab.Screen
+        name="CretePost"
+        component={CreatePostScreen}
+        options={{
+          title: 'Створити публикацію',
+          tabBarShowLabel: false,
+          headerShown: true,
+          headerLeft: () => (
+            <HeaderBackButton
+              label="Hello"
+              onPress={() => navigation.navigate('Posts')}
+            />
+          ),
+          tabBarIcon: ({ focused, size, color }) => {
+            return (
+              <View>
+                <View
+                  style={{
+                    width: 70,
+                    height: 40,
+                    backgroundColor: 'tomato',
+                    borderRadius: 20,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <AntDesign name="plus" size={13} color="#FFFFFF" />
+                </View>
+              </View>
+            );
+          },
+        }}
+      />
+      <MainTab.Screen
+        name="UserProfile"
+        component={Profile}
+        options={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused, size, color }) => {
+            return (
+              <Feather name="user" size={24} color="rgba(33, 33, 33, 0.8)" />
+            );
+          },
+        }}
+      />
+    </MainTab.Navigator>
+  );
+}
--- screens/mainScreen/CreatePostsScreen.js
+++ screens/mainScreen/CreatePostsScreen.js
@@ -1,18 +1,65 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
+
 import { Camera } from 'expo-camera';
 import { MaterialIcons } from '@expo/vector-icons';
-export default function NewPostScreen({ navigation }) {
+import { useState } from 'react';
+
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
+
+export default function NewPostScreen() {
+  const [camera, setCamera] = useState(null);
+  const [photo, setPhoto] = useState('');
+  const takePhoto = async () => {
+    const photo = await camera.takePictureAsync();
+    setPhoto(photo.uri);
+    console.log(photo);
+  };
+  const addPhoto = async (e) => {
+    if (photo === '') {
+      const photoOnDevice = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (!photoOnDevice.canceled) {
+        setPhoto((prevState) => ({ ...prevState, photo: photoOnDevice.uri }));
+      }
+    } else {
+      setPhoto((prevState) => ({ ...prevState, photo: '' }));
+    }
+  };
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera}>
-        <TouchableOpacity style={{}} onPress={() => {}}>
-          <View style={styles.iconBg}>
-            <MaterialIcons name="camera-alt" size={24} color="#BDBDBD" />
+      {photo === '' ? (
+        <View style={styles.photoWrapp}>
+          <TouchableOpacity onPress={takePhoto}>
+            <View style={styles.iconBg}>
+              <MaterialIcons name="camera-alt" size={24} color="#BDBDBD" />
+            </View>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <Camera style={styles.camera} ref={setCamera}>
+          <View style={styles.takePhotoContaainer}>
+            <Image
+              sourse={{ uri: photo }}
+              style={{ height: 100, width: 100 }}
+            />
           </View>
-        </TouchableOpacity>
-      </Camera>
-      <Text style={styles.cameraText}>Завантажте фото</Text>
+
+          <TouchableOpacity onPress={takePhoto}>
+            <View style={styles.iconBg}>
+              <MaterialIcons name="camera-alt" size={24} color="#BDBDBD" />
+            </View>
+          </TouchableOpacity>
+        </Camera>
+      )}
+
+      <TouchableOpacity onPress={addPhoto}>
+        <Text style={styles.cameraText}>Завантажте фото</Text>
+      </TouchableOpacity>
       {/* <TouchableOpacity onPress={() => navigation.navigate('Login')}>
         <Image
           style={styles.logoutIcon}
@@ -34,44 +81,54 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     paddingTop: 32,
   },
-  header: {
-    position: 'absolute',
-    flexDirection: 'row',
-    left: 0,
-    right: 0,
-    top: 0,
-    minWidth: 375,
-    minHeight: 88,
-    paddingTop: 55,
-    paddingBottom: 16,
-    paddingHorizontal: 16,
-    borderWidth: 1,
-    borderTop: 1,
-    alignItems: 'center',
-    borderColor: '#BDBDBD',
-    boxShadow: '0px -0.5px 0px rgba(0, 0, 0, 0.3)',
-    backdropFilter: 'blur(13.5914px)',
-  },
-  headerTxt: {
+  // header: {
+  //   position: 'absolute',
+  //   flexDirection: 'row',
+  //   left: 0,
+  //   right: 0,
+  //   top: 0,
+  //   minWidth: 375,
+  //   minHeight: 88,
+  //   paddingTop: 55,
+  //   paddingBottom: 16,
+  //   paddingHorizontal: 16,
+  //   borderWidth: 1,
+  //   borderTop: 1,
+  //   alignItems: 'center',
+  //   borderColor: '#BDBDBD',
+  //   boxShadow: '0px -0.5px 0px rgba(0, 0, 0, 0.3)',
+  //   backdropFilter: 'blur(13.5914px)',
+  // },
+  // headerTxt: {
+  //   justifyContent: 'center',
+  //   fontFamily: 'Roboto',
+  //   fontStyle: 'normal',
+  //   fontWeight: '500',
+  //   fontSize: 17,
+  //   lineHeight: 22,
+  //   textAlign: 'center',
+  //   letterSpacing: -0.408,
+  //   color: '#212121',
+  //   marginLeft: 'auto',
+  //   marginRight: 'auto',
+  //   padding: 0,
+  // },
+  // logoutIcon: {
+  //   position: 'absolute',
+  //   right: 0,
+  //   bottom: -12,
+  //   width: 24,
+  //   height: 24,
+  // },
+  photoWrapp: {
+    backgroundColor: '#E8E8E8',
+    backgroundColor: 'rgba(232, 232, 232, 1)',
+    width: '100%',
+    height: 343,
     justifyContent: 'center',
-    fontFamily: 'Roboto',
-    fontStyle: 'normal',
-    fontWeight: '500',
-    fontSize: 17,
-    lineHeight: 22,
-    textAlign: 'center',
-    letterSpacing: -0.408,
-    color: '#212121',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    padding: 0,
-  },
-  logoutIcon: {
-    position: 'absolute',
-    right: 0,
-    bottom: -12,
-    width: 24,
-    height: 24,
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 8,
   },
   iconBg: {
     width: 60,
@@ -95,13 +152,20 @@ const styles = StyleSheet.create({
     borderRadius: 8,
   },
   cameraText: {
-    FontFamily: 'Roboto',
-    FontStyle: 'Regular',
-    FontSize: 16,
-    LineHeight: 19,
-    align: 'Left',
-    VerticalAlign: 'Top',
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    lineHeight: 19,
+
     color: '#BDBDBD',
     marginTop: 8,
+  },
+  takePhotoContaainer: {
+    position: 'absolute',
+    top: 30,
+    left: 30,
+    borderColor: '#fff',
+    borderWidth: 1,
+    // height: 200,
+    // width: 200,
   },
 });
